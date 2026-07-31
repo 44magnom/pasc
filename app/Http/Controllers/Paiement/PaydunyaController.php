903e4871d8e025ca7a222527aaadaf1e27b42803
44magnom <mniasse727@gmail.com>
2026-07-31 12:50:44 +0000

    binjour

--- app/Http/Controllers/Paiement/PaydunyaController.php
+++ app/Http/Controllers/Paiement/PaydunyaController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Forfait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 class PaydunyaController extends Controller
 {
